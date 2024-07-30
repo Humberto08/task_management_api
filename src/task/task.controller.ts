ef37feb9bb919c8fcab53a68d55fa93997b7c929
Humberto Luciano <humberto_luciano@hotmail.com>
2024-07-30 16:40:30 -0300

    feat: Implemanting route findById

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TaskDTO } from './dto/task.DTO';
 import { TaskService } from './task.service';
 
+
 @Controller('task')
 export class TaskController {
-    
+
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
   create(@Body() task: TaskDTO) {
     this.taskService.create(task);
-    this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): TaskDTO {
+    return this.taskService.findById(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { TaskDTO } from './dto/task.DTO';
 
 @Injectable()
@@ -9,6 +9,16 @@ export class TaskService {
   create(task: TaskDTO) {
     this.tasks.push(task);
     console.log(this.tasks);
+  }
+
+  findById(id: string): TaskDTO {
+    const foundTask = this.tasks.filter(task => task.id === id);
+
+    if (foundTask.length) {
+      return foundTask[0];
+    }
+
+    throw new NotImplementedException('Task with id ${id} not found');
   }
 }
 
