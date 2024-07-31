5fa7c09079ca77adf16ff7a96537126deb8aa63a
Humberto Luciano <humberto_luciano@hotmail.com>
2024-07-31 17:03:19 -0300

    feat: Implementig authDTO, config: auth.module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthResponseDTO } from './dto/auth.DTO';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    signIn(
+        @Body("username") username: string,
+        @Body("password") password: string,
+    ): AuthResponseDTO {
+        return this.authService.signIn(username, password);
+    }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [JwtModule, registerAsync({
+    global: true,
+    imports: [],
+    useFactory: async (confiService: ConfigService) => ({
+      secret: confiService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: +confiService.get<number>('JWT_EXPIRATION_TIME'),
+      }
+    }),
+    inject: [ConfigService]
+  })],
   providers: [AuthService],
   controllers: [AuthController]
 })
