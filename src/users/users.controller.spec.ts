078ef9e871ce75a5c748213b7d7f2e8b0404bb60
Humberto Luciano <humberto_luciano@hotmail.com>
2024-08-17 18:24:32 -0300

    test: create test users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserDTO } from './dto/user.DTO';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+
+  const mockUsersService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should successfully create a new user', async () => {
+      const userDTO: UserDTO = {
+        id: '1',
+        username: 'testuser',
+        password: 'password123',
+      };
+
+      // Mock the UsersService.create method to return the userDTO
+      mockUsersService.create.mockResolvedValue(userDTO);
+
+      const result = await controller.create(userDTO);
+      expect(result).toEqual(userDTO);
+      expect(mockUsersService.create).toHaveBeenCalledWith(userDTO);
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,9 @@ import { UserDTO } from './dto/user.DTO';
 
 @Controller('users')
 export class UsersController {
+  findByUserName(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
